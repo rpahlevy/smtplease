f0be6e261faa3a74d7127e2e9ccc5db90a40fe9b
Rio Pahlevy <43176811+rpahlevy@users.noreply.github.com>
2024-07-16 10:45:58 +0700

    update using async

--- app/database.py
+++ app/database.py
@@ -1,10 +1,10 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql+asyncpg://user:password@localhost/dbname"
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
@@ -8,20 +8,17 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # Dependency
-def get_db():
-  db = SessionLocal()
-  try:
+async def get_db():
+  async with SessionLocal() as db:
     yield db
-  finally:
-    db.close()
 
 @app.post("/emails/", response_model=schemas.Email)
-def create_email(email: schemas.EmailCreate, db: Session = Depends(get_db)):
-  return crud.create_email(db=db, email=email)
+async def create_email(email: schemas.EmailCreate, db: AsyncSession = Depends(get_db)):
+  return await crud.create_email(db=db, email=email)
 
 @app.get("/emails/{email_id}", response_model=schemas.Email)
-def read_email(email_id: int, db: Session = Depends(get_db)):
-  db_email = crud.get_email(db, email_id=email_id)
+async def read_email(email_id: int, db: AsyncSession = Depends(get_db)):
+  db_email = await crud.get_email(db, email_id=email_id)
   if db_email is None:
     raise HTTPException(status_code=404, detail="Email not found")
   return db_email
