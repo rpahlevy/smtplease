562ba13f5a8dd9e236822e4f992207c98c46e8ce
Rio Pahlevy <43176811+rpahlevy@users.noreply.github.com>
2024-07-17 10:20:19 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+print(os.getenv("DATABASE_URL"))
+print(os.getenv("DATABASE_PRIVATE_URL"))
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(
